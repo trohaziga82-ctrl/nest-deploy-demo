72d4b999444ad8e203f5f50a0705d8faebad1deb
Ziga <trohaziga82@gmail.com>
2026-02-07 11:34:45 +0100

    Valentinovo app ready for Render

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -10,7 +10,7 @@ export class AppController {
       <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Valentinovo 💖</title>
+        <title>E-asistent</title>
         <style>
           body {
             display: flex;
